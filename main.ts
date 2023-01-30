a3752d76fe4e298203adfe0889a679b3ae00bac3
Wideshrek <122555483+Wideshrek@users.noreply.github.com>
2023-01-30 12:21:34 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,22 @@
-OLED.init(128, 64)
-let PB = 0
-let PA = 0
-let ties = 0
+function updatescores () {
+    OLED.clear()
+    OLED.writeStringNewLine("Player A: " + PA)
+    OLED.newLine()
+    OLED.writeStringNewLine("Player B: " + PB)
+    OLED.newLine()
+    OLED.writeStringNewLine("Ties: " + ties)
+    OLED.newLine()
+    OLED.writeStringNewLine("Rounds: " + rounds)
+}
 let rounds = 0
-OLED.writeStringNewLine("GET OVER HERE AND PLAY WITH ME PLEASE")
+let ties = 0
+let PA = 0
+let PB = 0
+OLED.init(128, 64)
+PB = 0
+PA = 0
+ties = 0
+rounds = 0
+OLED.writeStringNewLine("GET YO BOOTY OVER HERE AND PLAY WITH MEEEEEE")
 basic.pause(2000)
+updatescores()
