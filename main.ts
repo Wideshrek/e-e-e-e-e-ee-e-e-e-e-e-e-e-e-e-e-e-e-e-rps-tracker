41d1bec791d944fecf1e565a7960834e9d73794a
Wideshrek <122555483+Wideshrek@users.noreply.github.com>
2023-01-30 12:10:57 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
-basic.forever(function () {
-	
-})
+OLED.init(128, 64)
+let PB = 0
+let PA = 0
+let ties = 0
+let rounds = 0
+OLED.writeStringNewLine("GET OVER HERE AND PLAY WITH ME PLEASE")
+basic.pause(2000)
